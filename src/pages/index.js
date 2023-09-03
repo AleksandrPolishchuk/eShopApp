ff72199b49ae39f46db1660f612f1e2f61d85bed
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-09-02 23:56:19 -0300

    add div to HomePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 export default function HomePage() {
-    return <div>test</div>
+    return (
+    <div className ="grid-halves h-screen-navbar">
+        <div className ="bg-teal border-right">
+            <div className = "column-padding">
+                <div className = "tablet-centered">
+                    <div className = "content-grid home-hero">
+                        Text
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
 }
