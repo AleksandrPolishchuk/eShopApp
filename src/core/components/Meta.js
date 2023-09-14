f48b82620633a5fbdd18f34a8d6f7279d51ca789
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-09-13 23:59:30 -0300

    add html to Meta

---
+++ src/core/components/Meta.js
@@ -0,0 +1,11 @@
+export default function Meta() {
+    return (
+        <>
+        <title>Epic Saas</title>
+        <meta
+            name="description"
+            content="content"
+            />
+        </>
+    );
+}
