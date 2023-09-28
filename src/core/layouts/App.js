72bb436dc1219056963f6bcbb14b49bf7e11e7cf
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-09-27 21:56:43 -0300

    bug fix global

--- src/core/layouts/App.js
+++ src/core/layouts/App.js
@@ -1,12 +1,1 @@
-import Meta from "src/core/components/Meta";
-import Navbar from "src/core/components/Navbar";
-
-export default function AppLayout({ children }) {
-    return (
-        <>
-        <Meta />
-        <Navbar />
-        {children}
-        </>
-    )
-}
+export default function AppLayout() {}
--- src/products/[slug].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function ProductPage() {
-    const router = useRouter();
-    console.log(router.query);
-
-    return <div>{router.query.slug}</div>
-}
