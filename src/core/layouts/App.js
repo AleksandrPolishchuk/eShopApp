389c338826af1ccd4a1716a16cc082e3f6202897
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-09-29 23:58:57 -0300

    add AppLayout to App.js

--- src/core/layouts/App.js
+++ src/core/layouts/App.js
@@ -1,8 +1,10 @@
+import Meta from "src/core/components/Meta";
 import Navbar from "src/core/components/Navbar";
 
 export default function AppLayout({ children }) {
   return (
     <>
+      <Meta />
       <Navbar />
       {children}
     </>
