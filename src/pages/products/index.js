26a5b811dbecde82bb4ca7ed5986713a5d256418
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-10-01 23:56:09 -0300

    add jsx for products/index

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,1 +1,13 @@
-export default function ProductsPage() {}
+export default function ProductsPage() {
+  return (
+    <>
+      <div className="section bg-blue">
+        <div className="container">
+          <div className="section-intro">
+            <h1>The latest products</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
