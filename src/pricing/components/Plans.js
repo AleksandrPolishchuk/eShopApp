9f6d56a9b448f44fd299af04d71386cb51133aa8
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-10-25 23:30:55 -0300

    add Plans

--- src/pricing/components/Plans.js
+++ src/pricing/components/Plans.js
@@ -1,1 +1,33 @@
-export default function Plans() {}
+export default function Plans({ plans }) {
+  const plan = plans[0];
+
+  return (
+    <div className="bg-salmon border-right">
+      <div className="column-padding centered">
+        <div className="callout-wrap">
+          <div className="plan">
+            <div className="plan-wrap">
+              <div className="plan-content">
+                <div className="plan-switch">
+                  M.
+                  <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider" />
+                  </label>
+                  Y.
+                </div>
+              </div>
+              <h2 className="plan-name">{plans.name}</h2>
+              <div>
+                J. ${plans.price} / {plans.interval}
+              </div>
+              <button className="large-button">
+                <div className="large-button-text">B. N.</div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
