aa6748ee283bc8dcfc51efa9af3ee29084098925
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-10-29 23:58:55 -0300

    add [priceId]

--- src/pages/api/checkout/[priceId].js
+++ src/pages/api/checkout/[priceId].js
@@ -1,3 +1,10 @@
 export default async function handler(req, res) {
   const { priceId } = req.query;
+
+  await stripe.checkout.sessions.create({
+    mode: "subscription",
+    payment_method_types: ["card"],
+    line_items: [{ price: priceId, quantity: 1 }],
+    success_url: `${SITE_URL}/success`,
+  });
 }
