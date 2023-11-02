aa9f53becd4993d24c53364d31aabf3ab9084d47
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-11-01 23:58:54 -0300

    add cancel_url field to [priceId]

--- src/pages/api/checkout/[priceId].js
+++ src/pages/api/checkout/[priceId].js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
   const { priceId } = req.query;
 
-  await stripe.checkout.sessions.create({
+  const session = await stripe.checkout.sessions.create({
     mode: "subscription",
     payment_method_types: ["card"],
     line_items: [{ price: priceId, quantity: 1 }],
     success_url: `${SITE_URL}/success`,
-    test_url: `${SITE_URL}/test`,
+    cancel_url: `${SITE_URL}/pricing`,
   });
+
+  res.send({ id: session.id });
 }
