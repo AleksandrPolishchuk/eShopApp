bd491b1eef465898d24c4e6ecf2c19c4d243b8b6
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-11-15 23:58:43 -0300

    add jsx to success

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,1 +1,13 @@
-export default function SuccessPage() {}
+import Image from "next/image";
+
+export default function SuccessPage() {
+  return (
+    <div className="section bg-pink h-screen">
+      <div className="container">
+        <div className="section-intro welcome">
+          <Image />
+        </div>
+      </div>
+    </div>
+  );
+}
