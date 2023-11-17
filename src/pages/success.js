15e04a5abe8aa76fb48366ef668d0949ae33e4ad
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-11-16 23:42:44 -0300

    add img & link to success

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,11 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
+import confetti from "../../public/assets/confetti.png";
 
 export default function SuccessPage() {
   return (
     <div className="section bg-pink h-screen">
       <div className="container">
         <div className="section-intro welcome">
-          <Image />
+          <Image
+            src={confetti}
+            height={200}
+            width={200}
+            alt="confetti"
+            className="confetti"
+          />
+          <h1>Success!</h1>
+          <p>Access granted</p>
+          <Link href="/login" className="large-button">
+            <div className="large-button-text">Go to Login</div>
+          </Link>
         </div>
       </div>
     </div>
