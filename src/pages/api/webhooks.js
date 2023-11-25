6992644ef8dc0fa87af528cc8ed9efe13d15d4f0
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-11-24 23:48:59 -0300

    add config to webhooks

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -1,6 +1,12 @@
 import getRawBody from "raw-body";
 import { stripe } from "src/pricing/utils/stripe";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function handler(req, res) {
   const signature = req.headers["stripe-signature"];
   const signingSecret = process.env.STRIPE_SIGNING_SECRET;
@@ -9,7 +15,8 @@ export default async function handler(req, res) {
   let event;
 
   try {
-    stripe.webhooks.constructEvent(rawBody, signature, signingSecret);
+    const rawBody = await getRawBody(req, { limit: "2mb" });
+    event = stripe.webhooks.constructEvent(rawBody, signature, signingSecret);
   } catch (error) {
     console.log("WebHook signature verification failed.");
     return res.status(400).end();
