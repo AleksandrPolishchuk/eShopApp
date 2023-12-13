ad659b4fba4f48d9efb7a34ab38c613cb10b56f4
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-12-12 23:58:25 -0300

    add supabase deps to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import AppLayout from "src/core/layouts/App";
 import "src/styles/globals.css";
 
